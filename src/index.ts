5aae6565fa69728ebb622434ae5afdf13860e01d
ramos <rramos@fi.uba.ar>
2023-11-29 23:13:05 -0300

    Corregidos errores de TypeScript y listo para despliegue

--- src/index.ts
+++ src/index.ts
@@ -35,148 +35,210 @@ interface MyContext extends TelegrafContext {
 const paymentWizard = new Scenes.WizardScene<MyContext>(
   'payment-wizard',
   async (ctx) => {
-    await ctx.reply(
-      'Hola! Bienvenido al bot de Erexit 3D. ¿Ya has realizado el pago para entrar a nuestra comunidad?',
-      Markup.inlineKeyboard([
-        Markup.button.callback('Sí', 'yes'),
-        Markup.button.callback('No', 'no')
-      ])
-    );
-    (ctx.wizard.state as MyWizardSession).userState = UserState.ASKED_PAYMENT;
-    return ctx.wizard.next();
+    try {
+      await ctx.reply(
+        'Hola! Bienvenido al bot de Erexit 3D. ¿Ya has realizado el pago para entrar a nuestra comunidad?',
+        Markup.inlineKeyboard([
+          Markup.button.callback('Sí', 'yes'),
+          Markup.button.callback('No', 'no')
+        ])
+      );
+      (ctx.wizard.state as MyWizardSession).userState = UserState.ASKED_PAYMENT;
+      return ctx.wizard.next();
+    } catch (error) {
+      console.error('Error en la primera etapa del wizard:', error);
+    }
   },
   async (ctx) => {
     // Este paso se manejará con botones, por lo que no es necesario aquí
   },
   async (ctx) => {
-    await ctx.reply(
-      'Perfecto, elige el método de pago:',
-      Markup.inlineKeyboard([
-        Markup.button.callback('Paypal', 'paypal'),
-        Markup.button.callback('MercadoPago', 'mercadopago'),
-        Markup.button.callback('Patreon', 'patreon')
-      ])
-    );
-    (ctx.wizard.state as MyWizardSession).userState = UserState.SELECTED_PAYMENT_METHOD;
-    return ctx.wizard.next();
+    try {
+      await ctx.reply(
+        'Perfecto, elige el método de pago:',
+        Markup.inlineKeyboard([
+          Markup.button.callback('Paypal', 'paypal'),
+          Markup.button.callback('MercadoPago', 'mercadopago'),
+          Markup.button.callback('Patreon', 'patreon')
+        ])
+      );
+      (ctx.wizard.state as MyWizardSession).userState = UserState.SELECTED_PAYMENT_METHOD;
+      return ctx.wizard.next();
+    } catch (error) {
+      console.error('Error en la selección del método de pago:', error);
+    }
   },
   async (ctx) => {
-    // Recoger datos del usuario
-    await ctx.reply('Perfecto, ahora te voy a pedir tus datos. Ingresa tu nombre completo por favor');
-    (ctx.wizard.state as MyWizardSession).userState = UserState.COLLECTED_INFO;
-    return ctx.wizard.next();
+    try {
+      await ctx.reply('Perfecto, ahora te voy a pedir tus datos. Ingresa tu nombre completo por favor');
+      (ctx.wizard.state as MyWizardSession).userState = UserState.COLLECTED_INFO;
+      return ctx.wizard.next();
+    } catch (error) {
+      console.error('Error al recoger datos del usuario:', error);
+    }
   },
   async (ctx: MyContext) => {
-    // Solicitar correo electrónico
-    await ctx.reply('Excelente, ahora tu dirección de mail');
-    (ctx.wizard.state as MyWizardSession).userState = UserState.COLLECTED_INFO;
-    return ctx.wizard.next();
+    try {
+      await ctx.reply('Excelente, ahora tu dirección de mail');
+      (ctx.wizard.state as MyWizardSession).userState = UserState.COLLECTED_INFO;
+      return ctx.wizard.next();
+    } catch (error) {
+      console.error('Error al solicitar el correo electrónico:', error);
+    }
   },
   async (ctx) => {
-    // Enlace de pago y confirmación
-    await ctx.reply('Ahora por favor realiza el pago a través del siguiente enlace>');
-    // Aquí deberías insertar el enlace de pago
-    await ctx.reply('Ingresa "ok" si ya realizaste el pago');
-    (ctx.wizard.state as MyWizardSession).userState = UserState.PAYMENT_VERIFICATION;
-    return ctx.wizard.next();
+    try {
+      await ctx.reply('Ahora por favor realiza el pago a través del siguiente enlace>');
+      // Aquí deberías insertar el enlace de pago
+      await ctx.reply('Ingresa "ok" si ya realizaste el pago');
+      (ctx.wizard.state as MyWizardSession).userState = UserState.PAYMENT_VERIFICATION;
+      return ctx.wizard.next();
+    } catch (error) {
+      console.error('Error en la etapa de enlace de pago y confirmación:', error);
+    }
   },
   async (ctx) => {
-    // Verificación final y enlace al grupo
-    // Aquí deberías implementar la lógica de verificación del pago
-    await ctx.reply('Excelente! ya estás suscrito. Ingresá al grupo de telegram con este link:-------');
-    // Finalizar la escena
-    return ctx.scene.leave();
+    try {
+      await ctx.reply('Excelente! ya estás suscrito. Ingresá al grupo de telegram con este link:-------');
+      // Finalizar la escena
+      return ctx.scene.leave();
+    } catch (error) {
+      console.error('Error en la verificación final y enlace al grupo:', error);
+    }
   }
 );
 
-const rateLimit = (limit: number, interval: number): MiddlewareFn<Context> => {
-  let lastCalled = Date.now();
-
-  return async (ctx, next) => {
-    if (Date.now() - lastCalled < interval) {
-      await ctx.reply(`Por favor, espera ${interval / 1000} segundos antes de intentar de nuevo.`);
-      return;
-    }
-    lastCalled = Date.now();
-    await next();
-  };
-};
-
-bot.use(rateLimit(1, 5000)); // Limita a 1 solicitud cada 5 segundos
-
 bot.action('yes', async (ctx) => {
-  const myCtx = ctx as unknown as MyContext; // Conversión doble
-  await myCtx.reply(
-    'Has seleccionado Sí. Por favor, selecciona el método de pago que utilizaste:',
-    Markup.inlineKeyboard([
-      Markup.button.callback('Paypal', 'paid_paypal'),
-      Markup.button.callback('MercadoPago', 'paid_mercadopago'),
-      Markup.button.callback('Patreon', 'paid_patreon')
-    ])
-  );
-  // Asumiendo que quieres mantener el usuario en el mismo paso del asistente
-  return myCtx.wizard.selectStep(myCtx.wizard.cursor);
+  const myCtx = ctx as unknown as MyContext;
+  try {
+    await myCtx.reply(
+      'Has seleccionado Sí. Por favor, selecciona el método de pago que utilizaste:',
+      Markup.inlineKeyboard([
+        Markup.button.callback('Paypal', 'paid_paypal'),
+        Markup.button.callback('MercadoPago', 'paid_mercadopago'),
+        Markup.button.callback('Patreon', 'paid_patreon')
+      ])
+    );
+    return myCtx.wizard.selectStep(myCtx.wizard.cursor);
+  } catch (error) {
+    console.error('Error al manejar acción "yes":', error);
+  }
 });
 
 bot.action('no', async (ctx) => {
-  const myCtx = ctx as unknown as MyContext; // Conversión doble
-  await myCtx.reply('Has seleccionado No. Por favor, realiza el pago para continuar.');
-  return myCtx.scene.leave();
+  const myCtx = ctx as unknown as MyContext;
+  try {
+    await myCtx.reply('Has seleccionado No. Por favor, realiza el pago para continuar.');
+    return myCtx.scene.leave();
+  } catch (error) {
+    console.error('Error al manejar acción "no":', error);
+  }
 });
 
 bot.action('paypal', async (ctx) => {
-  const myCtx = ctx as unknown as MyContext; // Conversión doble
-  await myCtx.reply('Has seleccionado Paypal. Por favor, ingresa tus datos.');
-  return myCtx.wizard.selectStep(3);
+  const myCtx = ctx as unknown as MyContext;
+  try {
+    await myCtx.reply('Has seleccionado Paypal. Por favor, ingresa tus datos.');
+    return myCtx.wizard.selectStep(3);
+  } catch (error) {
+    console.error('Error al manejar acción "paypal":', error);
+  }
 });
 
 bot.action('mercadopago', async (ctx) => {
-  const myCtx = ctx as unknown as MyContext; // Conversión doble
-  await myCtx.reply('Has seleccionado MercadoPago. Por favor, ingresa tus datos.');
-  return myCtx.wizard.selectStep(3);
+  const myCtx = ctx as unknown as MyContext;
+  try {
+    await myCtx.reply('Has seleccionado MercadoPago. Por favor, ingresa tus datos.');
+    return myCtx.wizard.selectStep(3);
+  } catch (error) {
+    console.error('Error al manejar acción "mercadopago":', error);
+  }
 });
 
 bot.action('patreon', async (ctx) => {
-  const myCtx = ctx as unknown as MyContext; // Conversión doble
-  await myCtx.reply('Has seleccionado Patreon. Por favor, ingresa tus datos.');
-  return myCtx.wizard.selectStep(3);
+  const myCtx = ctx as unknown as MyContext;
+  try {
+    await myCtx.reply('Has seleccionado Patreon. Por favor, ingresa tus datos.');
+    return myCtx.wizard.selectStep(3);
+  } catch (error) {
+    console.error('Error al manejar acción "patreon":', error);
+  }
 });
 
 bot.action('paid_paypal', async (ctx) => {
   const myCtx = ctx as unknown as MyContext;
-  await myCtx.reply('Has indicado que pagaste con Paypal. Procederemos a verificar tu pago.');
-  // Aquí puedes añadir lógica para manejar la verificación de pago con Paypal
-  // Por ejemplo, pasar al siguiente paso del asistente
-  return myCtx.wizard.next();
+  try {
+    await myCtx.reply('Has indicado que pagaste con Paypal. Procederemos a verificar tu pago.');
+    // Aquí puedes añadir lógica para manejar la verificación de pago con Paypal
+    return myCtx.wizard.next();
+  } catch (error) {
+    console.error('Error al manejar acción "paid_paypal":', error);
+  }
 });
 
 bot.action('paid_mercadopago', async (ctx) => {
   const myCtx = ctx as unknown as MyContext;
-  await myCtx.reply('Has indicado que pagaste con MercadoPago. Procederemos a verificar tu pago.');
-  // Lógica similar para MercadoPago
-  return myCtx.wizard.next();
+  try {
+    await myCtx.reply('Has indicado que pagaste con MercadoPago. Procederemos a verificar tu pago.');
+    // Lógica similar para MercadoPago
+    return myCtx.wizard.next();
+  } catch (error) {
+    console.error('Error al manejar acción "paid_mercadopago":', error);
+  }
 });
 
 bot.action('paid_patreon', async (ctx) => {
   const myCtx = ctx as unknown as MyContext;
-  await myCtx.reply('Has indicado que pagaste con Patreon. Procederemos a verificar tu pago.');
-  // Lógica similar para Patreon
-  return myCtx.wizard.next();
+  try {
+    await myCtx.reply('Has indicado que pagaste con Patreon. Procederemos a verificar tu pago.');
+    // Lógica similar para Patreon
+    return myCtx.wizard.next();
+  } catch (error) {
+    console.error('Error al manejar acción "paid_patreon":', error);
+  }
 });
 
 bot.use(session());
 const stage = new Scenes.Stage<MyContext>([paymentWizard], { default: 'payment-wizard' });
 bot.use(stage.middleware() as MiddlewareFn<Context>);
 
-bot.command('about', about());
+bot.command('about', async (ctx) => {
+  try {
+    await about()(ctx);
+  } catch (error) {
+    console.error('Error al ejecutar el comando "about":', error);
+    await ctx.reply('Ocurrió un error al procesar el comando "about".');
+  }
+});
+
 bot.command('start', async (ctx) => {
   try {
     (ctx as unknown as MyContext).scene.enter('payment-wizard');
   } catch (error) {
-    console.error(error);
+    console.error('Error al iniciar el comando "start":', error);
     await ctx.reply('Ocurrió un error, por favor intenta de nuevo.');
   }
 });
+
+const rateLimit = (limit: number, interval: number): MiddlewareFn<Context> => {
+  let lastCalled = Date.now();
+
+  return async (ctx, next) => {
+    try {
+      if (Date.now() - lastCalled < interval) {
+        await ctx.reply(`Por favor, espera ${interval / 1000} segundos antes de intentar de nuevo.`);
+        return;
+      }
+      lastCalled = Date.now();
+      await next();
+    } catch (error) {
+      console.error('Error en el middleware de rate limit:', error);
+    }
+  };
+};
+
+bot.use(rateLimit(1, 5000)); // Limita a 1 solicitud cada 5 segundos
+
 
 if (process.env.ENVIRONMENT !== 'production') {
   development(bot);
